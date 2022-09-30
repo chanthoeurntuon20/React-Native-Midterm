533b5d3da9219251d4e442d3e0b20e368f3bf7d4
chanthoeurntuon20 <chanthoeurntuon20@gamil.com>
2022-09-30 11:18:18 +0700

    update code

---
+++ data/QuizData.js
@@ -0,0 +1,27 @@
+export default data = [
+    {
+        question: "I ……… football every Saturday afternoon.",
+        options: ["am playing","am play","play","playing"],
+        correct_option: "play"
+    },
+    {
+        question: "You should ______ your homework ",
+        options: ["make","do","give"],
+        correct_option: "do"
+    },
+    {
+        question: "He's interested ______ learning Spanish.",
+        options: ["on","in","for","to"],
+        correct_option: "in"
+    },
+    {
+        question: "You ______ the cleaning. I would have done it tonight.",
+        options: ["needn'thave done","couldn't have done","can't have done","Don't know"],
+        correct_option: "needn'thave done"
+    },
+    {
+        question: "I don't like coffee.______ do I.",
+        options: ["So","Neither","Either","No"],
+        correct_option: "Neither"
+    }
+]
